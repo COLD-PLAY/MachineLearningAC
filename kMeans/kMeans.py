bb1688625d96503f7cb9d520d56ef7db58d1517b
廖舟 <mi_xi_shi_zi@163.com>
2017-09-10 11:11:57 +0800

    add more

--- kMeans/kMeans.py
+++ kMeans/kMeans.py
@@ -1,57 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # 加载数据
 def loadDataSet(filename):
-	dataSet = []
+    dataSet = []
 
-	fr = open(filename, 'r')
-	for line in fr.readlines():
-		curLine = line.strip().split('\t')
-		fltLine = map(float, curLine)
-		dataSet.append(fltLine)
+    fr = open(filename, 'r')
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)
+        dataSet.append(fltLine)
 
-	fr.close()
-	# print(dataSet)
-	return np.mat(dataSet)
+    fr.close()
+    # print(dataSet)
+    return np.mat(dataSet)
+
 
 def kMeans(dataSet, k):
-	m, n = np.shape(dataSet)
-	MU = np.mat(np.random.random((k, n)))
+    m, n = np.shape(dataSet)
+    MU = np.mat(np.random.random((k, n)))
 
-	print(MU)
+    print(MU)
 
-	C = minDis(dataSet, MU)
-	print(C)
+    C = minDis(dataSet, MU)
+    print(C)
+
 
 def minDis(X, MU):
-	m = np.shape(X)[0]
-	k = np.shape(MU)[0]
+    m = np.shape(X)[0]
+    k = np.shape(MU)[0]
 
-	min_d = np.inf
+    min_d = np.inf
 
-	C = np.mat(np.zeros((m, 1)))
+    C = np.mat(np.zeros((m, 1)))
 
-	for i in range(0, m):
-		for j in range(0, k):
-			dis = (X[i, :] - MU[j, :]) * (X[i, :] - MU[j, :]).T
-			if min_d > dis:
-				min_d = dis
-				C[i, 0] = j
-	return C
+    for i in range(0, m):
+        for j in range(0, k):
+            dis = (X[i, :] - MU[j, :]) * (X[i, :] - MU[j, :]).T
+            if min_d > dis:
+                min_d = dis
+                C[i, 0] = j
+    return C
 
-	
+
 def plotData(dataSet):
-	plt.plot(dataSet[:, 0], dataSet[:, 1], 'o')
-	plt.show()
+    plt.plot(dataSet[:, 0], dataSet[:, 1], 'o')
+    plt.show()
+
 
 if __name__ == '__main__':
-	dataSet = loadDataSet('testSet.txt')
-	kMeans(dataSet, 4)
-	# plotData(dataSet)
+    dataSet = loadDataSet('C:\\Users\\COLDPLAY\\Desktop\\GitHub\\MachineLearningAC\\KMeans\\testSet.txt')
+    kMeans(dataSet, 4)
+    # plotData(dataSet)
 
-	# a = np.mat([[1, 2, 3], [4, 5, 6]])
-	# b = np.mat([[1, 2, 3], [4, 5, 7]])
-	# for i in range(0, 2):
-	# 	c = a[i, :] - b[i, :]
-	# 	print(c)
+    # a = np.mat([[1, 2, 3], [4, 5, 6]])
+    # b = np.mat([[1, 2, 3], [4, 5, 7]])
+    # for i in range(0, 2):
+    # 	c = a[i, :] - b[i, :]
+    # 	print(c)
