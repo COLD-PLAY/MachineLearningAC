977edb3e154b1af630f85e2eae17625c9e44cc15
廖舟 <mi_xi_shi_zi@163.com>
2017-09-11 16:05:34 +0800

    finish kMeans algorithm

--- kMeans/kMeans.py
+++ kMeans/kMeans.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,52 +10,96 @@ def loadDataSet(filename):
     fr = open(filename, 'r')
     for line in fr.readlines():
         curLine = line.strip().split('\t')
-        for i in range(0, len(curLine)):
-            curLine[i] = float(curLine[i])
+        # fltLine = map(float, curLine)
+        fltLine = []
+        for i in range(len(curLine)):
+            fltLine.append(float(curLine[i]))
 
-        print(curLine)
-        dataSet.append(curLine)
-
-    fr.close()
-    # print(dataSet)
+        # print(fltLine)
+        dataSet.append(fltLine)
+    
     return np.mat(dataSet)
 
-
+# algorithm kernel
 def kMeans(dataSet, k):
     m, n = np.shape(dataSet)
-    MU = np.mat(np.random.random((k, n)))
+    Mu = initMu(dataSet, k)
 
-    print(MU)
+    # print(Mu)
 
-    C = minDis(dataSet, MU)
-    print(C)
+    C = minDis(dataSet, Mu)
+    
+    for i in range(1000):
+        Mu = descent(dataSet, Mu, C)
+        C = minDis(dataSet, Mu)    
+        
+    plotData(dataSet, C, Mu)
 
+# find the optimization of Mu to make the total distance minimization
+def descent(dataSet, Mu, C):
+    k, n = Mu.shape
+    m = C.shape[0]
 
-def minDis(X, MU):
+    Mu_n = np.mat(np.zeros((k, n)))
+    for j in range(k):
+        MuJ = np.mat(np.zeros((1, n)))
+        count = 0
+        for i in range(m):
+            if C[i, 0] == j:
+                count += 1
+                MuJ += dataSet[i, :]
+        Mu_n[j, :] = MuJ/count
+    return Mu_n
+            
+# calcute distance of vecA and vecB
+def distEclud(vecA, vecB):
+    return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
+
+# init the Mu randomly between the minX and maxX
+def initMu(dataSet, k):
+    m, n = dataSet.shape
+
+    Mu = np.mat(np.zeros((k, n)))
+    for j in range(n):
+        minJ = min(dataSet[:, j])
+        rangeJ = float(max(dataSet[:, j]) - minJ)
+
+        Mu[:, j] = minJ + rangeJ*np.random.rand(k, 1)
+    return Mu
+
+# calculate the most close Mu[i] for each X[i]
+def minDis(X, Mu):
     m = np.shape(X)[0]
-    k = np.shape(MU)[0]
-
-    min_d = np.inf
+    k = np.shape(Mu)[0]
 
     C = np.mat(np.zeros((m, 1)))
 
     for i in range(0, m):
-        for j in range(0, k):
-            dis = (X[i, :] - MU[j, :]) * (X[i, :] - MU[j, :]).T
+        min_d = np.inf
+        for j in range(k):
+            # dis = (X[i, :] - Mu[j, :]) * (X[i, :] - Mu[j, :]).T
+            dis = distEclud(Mu[j, :], X[i, :])
             if min_d > dis:
                 min_d = dis
                 C[i, 0] = j
     return C
 
+# plot the data which had been classified to k's kinds
+def plotData(dataSet, C, Mu, i = 1000):
+    k = Mu.shape[0]
+    m, n = dataSet.shape
 
-def plotData(dataSet):
-    plt.plot(dataSet[:, 0], dataSet[:, 1], 'o')
+    plt.title('i: %d' % i)
+    for i in range(m):
+        plt.plot(dataSet[i, 0], dataSet[i, 1], 'C%do' % C[i])
+        
+    for i in range(k):
+        plt.plot(Mu[i, 0], Mu[i, 1], 'C%d+' % i)
     plt.show()
-
 
 if __name__ == '__main__':
     dataSet = loadDataSet('kMeans/testSet.txt')
-    kMeans(dataSet, 4)
+    kMeans(dataSet, 2)
     # plotData(dataSet)
 
     # print(dataSet)
