de1792b26eb906af13631729a1409777d6a75db6
廖舟 <mi_xi_shi_zi@163.com>
2017-09-14 19:58:43 +0800

    change the encode of 23.txt and finish sift spam email with nb algorithm

--- naiveBayes/naiveBayes.py
+++ naiveBayes/naiveBayes.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 def loadDataSet():
-    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
-                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
-                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
-                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0, 1, 0, 1, 0, 1]    #1 is abusive, 0 not
-    return postingList, classVec
+	postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+				 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+				 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+				 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+				 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+				 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+	classVec = [0, 1, 0, 1, 0, 1]	#1 is abusive, 0 not
+	return postingList, classVec
 
 def createVacabList(dataSet):
 	vocabSet = set([])
@@ -17,13 +18,14 @@ def createVacabList(dataSet):
 		vocabSet = vocabSet | set(document)
 	return list(vocabSet)
 
+# 词集模型
 def setOfWords2Vec(vocabList, inputSet):
-    returnVec = [0]*len(vocabList)
-    for word in inputSet:
-        if word in vocabList:
-            returnVec[vocabList.index(word)] = 1
-        else: print("the word: %s is not in my Vocabulary!" % word)
-    return returnVec
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] = 1
+		else: print("the word: %s is not in my Vocabulary!" % word)
+	return returnVec
 
 # train the classifier with trainMatrix and its trainCategory
 def trainNB0(trainMatrix, trainCategory):
@@ -38,7 +40,7 @@ def trainNB0(trainMatrix, trainCategory):
 	p0Denom = 2.0 ; p1Denom = 2.0
 
 	for i in range(numTrainDocs):
-		if trainCategory[i] == 1:
+		if trainCategory[i] > 0:
 			p1Num += trainMatrix[i]
 			p1Denom += sum(trainMatrix[i])
 		else:
@@ -51,6 +53,7 @@ def trainNB0(trainMatrix, trainCategory):
 	# p1Vect = np.log(p1Num/p1Denom)
 	
 	return p0Vect, p1Vect, pAbusive
+
 
 def classifyNB(vec2Classify, p0Vect, p1Vect, pClass1):
 	p1 = np.sum(vec2Classify * p1Vect) + np.log(pClass1)
@@ -79,10 +82,81 @@ def testingNB():
 
 	testEntry = ['stupid', 'garbage']
 	thisDoc = np.array(setOfWords2Vec(vocabList, testEntry))
-	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))		
+	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))	
+
+# 词袋模型，某些词出现不止一次可能会表达更多的信息
+def bagOfWords2VecMN(vocabList, inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] += 1
+		else:
+			pass
+		# else: print("the word: %s is not in my Vocabulary!" % word)
+	return returnVec
+
+def parseTxt(bigString):
+	pattern = re.compile(r'\w*', re.S)
+	words = re.findall(pattern, bigString)
+	words = [word.lower() for word in words if len(word) >= 3]
+
+	return words
+
+def loadEmailData():
+	emailData = []
+	for i in range(1, 21):
+		content = open('naiveBayes/email/ham/' + str(i) + '.txt').read()
+		words = parseTxt(content)
+		emailData.append(words)
+
+		content = open('naiveBayes/email/spam/' + str(i) + '.txt').read()
+		words = parseTxt(content)
+		emailData.append(words)
+	
+	emailClasses = [i%2 for i in range(40)]
+	# print(len(emailData), ' ', len(emailClasses))
+
+	return emailData, emailClasses
+
+def spamTest():
+	emailData, emailClasses = loadEmailData()
+	vocabList = createVacabList(emailData)
+	error = 0 ; count = 0
+
+	trainMatrix = []
+	for email in emailData:
+		# trainMatrix.append(bagOfWords2VecMN(vocabList, email))
+		trainMatrix.append(bagOfWords2VecMN(vocabList, email))
+
+	p0V, p1V, pSp = trainNB0(trainMatrix, emailClasses)	
+	print(p0V, p1V, pSp)
+	
+	for i in range(21, 26):
+		ham = open('naiveBayes/email/ham/' + str(i) + '.txt').read()
+		email = parseTxt(ham)
+		emailVec = np.array(bagOfWords2VecMN(vocabList, email))
+		# print(emailVec)
+		
+		res = classifyNB(emailVec, p0V, p1V, pSp)
+		print('the %dth of ham classified as: ' % i, res)
+
+		if res == 1: error += 1
+
+		spam = open('naiveBayes/email/spam/' + str(i) + '.txt').read()
+		email = parseTxt(spam)
+		emailVec = np.array(bagOfWords2VecMN(vocabList, email))
+
+		res = classifyNB(emailVec, p0V, p1V, pSp)
+		print('the %dth of spam classified as: ' % i, res)
+
+		if res == 0: error += 1		
+
+		count += 2
+	print('the error rate is: ', error/float(count))
 
 def main():
 	testingNB()
 
 if __name__ == '__main__':
-	main()
+	# main()
+	spamTest()
